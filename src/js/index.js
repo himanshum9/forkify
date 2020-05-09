d87fc50b922dc83e5fc5e11b496ce7453e3bb77f
himanshu.mehta09 <himanshumehta.hm9@gmail.com>
2020-05-09 23:11:30 +0530

    showing all recipes

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,28 @@
 import Search from './models/Search';
+import {elements} from './views/base';
+import * as searchView from './views/searchView';
+
+/** Global State of the app
+*Search Object
+*Current Recipe Object
+*Shopping list Object
+*Liked Recipe
+*/
+const state = {};
+const controlSearch = async() => {
+	const query = searchView.getInput();
+	if (query) {
+	state.search = new Search(query);
+	state.search.recipe = await state.search.getResults();
+	console.log(state.search.recipe);
+	}
+}
+elements.searchForm.addEventListener('submit', e=>{
+	e.preventDefault();
+	controlSearch();
+	alert('hey');
+	
+});
 const search = new Search('pizza');
 const recipe = search.getResults();
 console.log(recipe);
---
+++ src/js/views/base.js
@@ -0,0 +1,5 @@
+export const elements = {
+	searchForm: document.querySelector('.search'),
+	searchInput: document.querySelector('.search__field'),
+	searchResArray: document.querySelector('.results__list')
+}
